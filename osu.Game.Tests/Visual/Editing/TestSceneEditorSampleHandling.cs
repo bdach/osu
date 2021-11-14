8f101ce8cf3a0a5f498ec01237d72a80cf89b3c0
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-11-14 13:14:57 +0100

    Add failing test coverage for samples not changing post-slider edit

--- osu.Game.Tests/Visual/Editing/TestSceneEditorSampleHandling.cs
+++ osu.Game.Tests/Visual/Editing/TestSceneEditorSampleHandling.cs
@@ -1,17 +1,20 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Testing;
+using osu.Game.Audio;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Rulesets.Osu.Objects.Drawables;
 using osu.Game.Skinning;
+using osuTK.Input;
 
 namespace osu.Game.Tests.Visual.Editing
 {
-    public class TestSceneEditorSamplePlayback : EditorTestScene
+    public class TestSceneEditorSampleHandling : EditorTestScene
     {
         protected override Ruleset CreateEditorRuleset() => new OsuRuleset();
 
@@ -45,6 +48,39 @@ namespace osu.Game.Tests.Visual.Editing
 
             AddAssert("non-looping samples are playing", () => onceOffSamples.Length == 4 && loopingSamples.All(s => s.Played || s.Playing));
             AddAssert("looping samples are not playing", () => loopingSamples.Length == 1 && loopingSamples.All(s => s.Played && !s.Playing));
+        }
+
+        [Test]
+        public void TestEditPreservesNestedSamples()
+        {
+            DrawableSlider slider = null;
+            List<HitSampleInfo> headSamples = null;
+
+            AddStep("get first slider", () =>
+            {
+                slider = Editor.ChildrenOfType<DrawableSlider>().OrderBy(s => s.HitObject.StartTime).First();
+                var headSkinnableSound = slider.HeadCircle.ChildrenOfType<SkinnableSound>().Single();
+                headSamples = headSkinnableSound.Samples.OfType<HitSampleInfo>().Select(s => s.With()).ToList();
+            });
+
+            AddStep("select slider", () =>
+            {
+                EditorBeatmap.SelectedHitObjects.Clear();
+                EditorBeatmap.SelectedHitObjects.Add(slider.HitObject);
+            });
+            AddStep("flip slider", () =>
+            {
+                InputManager.PressKey(Key.ControlLeft);
+                InputManager.Key(Key.J);
+                InputManager.ReleaseKey(Key.ControlLeft);
+            });
+
+            AddAssert("slider head samples unchanged", () =>
+            {
+                var headSkinnableSound = slider.HeadCircle.ChildrenOfType<SkinnableSound>().Single();
+                var newHeadSamples = headSkinnableSound.Samples.OfType<HitSampleInfo>().Select(s => s.With()).ToList();
+                return newHeadSamples.SequenceEqual(headSamples);
+            });
         }
     }
 }
