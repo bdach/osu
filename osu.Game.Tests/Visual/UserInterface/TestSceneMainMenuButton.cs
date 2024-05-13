aa291d2a8ef05a600c891c00639a1f405f111fe9
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-05-13 17:47:35 +0200

    fix tests though

--- osu.Game.Tests/Visual/UserInterface/TestSceneMainMenuButton.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneMainMenuButton.cs
@@ -1,18 +1,14 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Game.Database;
 using osu.Game.Graphics;
 using osu.Game.Localisation;
 using osu.Game.Online.API;
-using osu.Game.Online.API.Requests;
-using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Online.Metadata;
+using osu.Game.Online.Rooms;
 using osu.Game.Screens.Menu;
 using osuTK.Input;
 using Color4 = osuTK.Graphics.Color4;
@@ -42,38 +38,30 @@ namespace osu.Game.Tests.Visual.UserInterface
         [Test]
         public void TestBeatmapOfTheDayButton()
         {
-            BeatmapLookupCache lookupCache = null!;
-            AddStep("add lookup cache", () =>
+            AddStep("add button", () => Child = new BeatmapOfTheDayButton(@"button-default-select", new Color4(102, 68, 204, 255), _ => { }, 0, Key.D)
             {
-                Clear();
-                Add(lookupCache = new BeatmapLookupCache());
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                ButtonSystemState = ButtonSystemState.TopLevel,
             });
-            AddStep("add button", () => Add(new DependencyProvidingContainer
-            {
-                CachedDependencies = [(typeof(BeatmapLookupCache), lookupCache)],
-                RelativeSizeAxes = Axes.Both,
-                Child = new BeatmapOfTheDayButton(
-                    @"button-default-select", new Color4(102, 68, 204, 255), _ => { }, 0, Key.D)
-                {
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    ButtonSystemState = ButtonSystemState.TopLevel,
-                }
-            }));
 
             AddStep("set up API", () => dummyAPI.HandleRequest = req =>
             {
                 switch (req)
                 {
-                    case GetBeatmapsRequest getBeatmapsRequest:
-                        if (getBeatmapsRequest.BeatmapIds.Count != 1 || getBeatmapsRequest.BeatmapIds.SingleOrDefault() != 1001)
+                    case GetRoomRequest getRoomRequest:
+                        if (getRoomRequest.RoomId != 1234)
                             return false;
 
                         var beatmap = CreateAPIBeatmap();
                         beatmap.OnlineID = 1001;
-                        getBeatmapsRequest.TriggerSuccess(new GetBeatmapsResponse
+                        getRoomRequest.TriggerSuccess(new Room
                         {
-                            Beatmaps = new List<APIBeatmap> { beatmap }
+                            RoomID = { Value = 1234 },
+                            Playlist =
+                            {
+                                new PlaylistItem(beatmap)
+                            }
                         });
                         return true;
 
