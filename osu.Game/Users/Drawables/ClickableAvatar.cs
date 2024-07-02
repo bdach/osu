f098f9f828be37466bead9a31eceb5dc8aec6e7b
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-07-02 11:41:14 +0200

    clickableavatar too

--- osu.Game/Users/Drawables/ClickableAvatar.cs
+++ osu.Game/Users/Drawables/ClickableAvatar.cs
@@ -3,10 +3,13 @@
 
 using System.Linq;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Cursor;
 using osu.Framework.Input.Events;
+using osu.Framework.Logging;
+using osu.Game.Database;
 using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Cursor;
 using osu.Game.Localisation;
@@ -20,9 +23,11 @@ namespace osu.Game.Users.Drawables
     {
         public ITooltip<APIUser?> GetCustomTooltip() => showCardOnHover ? new UserCardTooltip() : new NoCardTooltip();
 
-        public APIUser? TooltipContent { get; }
+        // ReSharper disable once ConvertToAutoPropertyWithPrivateSetter
+        public APIUser? TooltipContent => apiUser;
 
-        private readonly APIUser? user;
+        private readonly IUser? user;
+        private APIUser? apiUser;
 
         private readonly bool showCardOnHover;
 
@@ -34,25 +39,53 @@ namespace osu.Game.Users.Drawables
         /// </summary>
         /// <param name="user">The user. A null value will get a placeholder avatar.</param>
         /// <param name="showCardOnHover">If set to true, the <see cref="UserGridPanel"/> will be shown for the tooltip</param>
-        public ClickableAvatar(APIUser? user = null, bool showCardOnHover = false)
+        public ClickableAvatar(IUser? user = null, bool showCardOnHover = false)
         {
-            if (user?.Id != APIUser.SYSTEM_USER_ID)
+            if (user?.OnlineID != APIUser.SYSTEM_USER_ID)
                 Action = openProfile;
 
             this.showCardOnHover = showCardOnHover;
 
-            TooltipContent = this.user = user ?? new GuestUser();
+            switch (user)
+            {
+                case APIUser onlineUser:
+                    this.user = apiUser = onlineUser;
+                    break;
+
+                case null:
+                    this.user = apiUser = new GuestUser();
+                    break;
+
+                default:
+                    this.user = user;
+                    break;
+            }
         }
 
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(UserLookupCache lookupCache)
         {
-            LoadComponentAsync(new DrawableAvatar(user), Add);
+            if (apiUser != null)
+                LoadComponentAsync(new DrawableAvatar(apiUser), Add);
+            else if (user != null)
+            {
+                lookupCache.GetUserAsync(user.OnlineID).ContinueWith(t =>
+                {
+                    if (t.Exception != null)
+                    {
+                        Logger.Log($"Error when looking up user for {nameof(UpdateableAvatar)}: {t.Exception}", LoggingTarget.Network);
+                        return;
+                    }
+
+                    apiUser = t.GetResultSafely();
+                    LoadComponentAsync(new DrawableAvatar(apiUser), Add);
+                });
+            }
         }
 
         private void openProfile()
         {
-            if (user?.Id > 1 || !string.IsNullOrEmpty(user?.Username))
+            if (user?.OnlineID > 1 || !string.IsNullOrEmpty(user?.Username))
                 game?.ShowUser(user);
         }
 
