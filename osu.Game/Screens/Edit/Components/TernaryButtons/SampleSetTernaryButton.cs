8d60e8830b3a5aefc5ae94ba793afa8c532dd681
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-10-16 12:49:54 +0200

    Fix code quality

--- osu.Game/Screens/Edit/Components/TernaryButtons/SampleSetTernaryButton.cs
+++ osu.Game/Screens/Edit/Components/TernaryButtons/SampleSetTernaryButton.cs
@@ -4,7 +4,6 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Audio;
 using osu.Game.Graphics;
