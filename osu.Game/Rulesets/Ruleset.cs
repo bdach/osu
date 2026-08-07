a1292100dd2aea13a1ba2b9ec9530a3e6e52ef5c
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-08-07 13:21:27 +0200

    promising start

--- osu.Game/Rulesets/Ruleset.cs
+++ osu.Game/Rulesets/Ruleset.cs
@@ -47,6 +47,7 @@ namespace osu.Game.Rulesets
         /// Version history:
         /// 2022.205.0   FramedReplayInputHandler.CollectPendingInputs renamed to FramedReplayHandler.CollectReplayInputs.
         /// 2022.822.0   All strings return values have been converted to LocalisableString to allow for localisation support.
+        /// TODO: probably bump this for the ruleset editor keybindings stuff
         /// </summary>
         public const string CURRENT_RULESET_API_VERSION = "2022.822.0";
 
@@ -306,9 +307,13 @@ namespace osu.Game.Rulesets
         public virtual string PlayingVerb => "Playing";
 
         /// <summary>
-        /// A list of available variant ids.
+        /// A list of available gameplay variant ids.
         /// </summary>
-        public virtual IEnumerable<int> AvailableVariants => new[] { 0 };
+        public virtual IEnumerable<int> GameplayVariants => new[] { 0 };
+
+        public IEnumerable<int> AllVariants => GameplayVariants.Append(EDITOR_VARIANT);
+
+        public const int EDITOR_VARIANT = int.MaxValue;
 
         /// <summary>
         /// Get a list of default keys for the specified variant.
@@ -328,7 +333,17 @@ namespace osu.Game.Rulesets
         /// </summary>
         /// <param name="variant">The variant.</param>
         /// <returns>A descriptive name of the variant.</returns>
-        public virtual LocalisableString GetVariantName(int variant) => string.Empty;
+        public virtual LocalisableString GetVariantName(int variant)
+        {
+            switch (variant)
+            {
+                default:
+                    return string.Empty;
+
+                case EDITOR_VARIANT:
+                    return EditorStrings.BeatmapEditor;
+            }
+        }
 
         /// <summary>
         /// Returns the ID of the variant that is applicable for the given <paramref name="beatmapInfo"/>, given the current active <paramref name="mods"/>.
